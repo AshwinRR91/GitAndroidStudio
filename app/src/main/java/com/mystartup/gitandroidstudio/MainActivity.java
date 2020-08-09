6ba92f4e3cdcca69acb1121e3a19b537c4adf38e
Ashwin <achchu.rai@gmail.com>
2020-08-09 13:05:49 +0530

    Log added to Main activity and vcs commited

--- app/src/main/java/com/mystartup/gitandroidstudio/MainActivity.java
+++ app/src/main/java/com/mystartup/gitandroidstudio/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("My Message","Adding to Github");
+        Log.i("My Message","Adding to Github oh");
     }
 }
